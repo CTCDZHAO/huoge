ca24a2a8881dbcbf06c0dc3e6ca7ac701759faeb
CTCDZHGAO <zhaohaiguang754@163.com>
2021-11-23 09:55:59 +0800

    模板替换

--- Hogwartshttp/test_token/api/get_token.py
+++ Hogwartshttp/test_token/api/get_token.py
@@ -1,24 +1,22 @@
+from string import Template
+
 import yaml
 
 from test_token.api.base_api import BaseApi
 import yaml
 
 class GetToken(BaseApi):
-    # _corpid="wwe3077ee291d6a898"
-    # _corp_secret="a-_vouMKTXFxX3G4yEWn8lsBNP2Cmq21oZWR24SFtJ0"
+    _corpid="wwe3077ee291d6a898"
+    _corp_secret="a-_vouMKTXFxX3G4yEWn8lsBNP2Cmq21oZWR24SFtJ0"
+    def template(self):
+        with open('../api/get_token.yaml') as f:
+            re=Template(f.read()).substitute(corpid=self._corpid,corpsecret=self._corp_secret)
+            return yaml.safe_load(re)
     def get_token(self):
-
-        # req={
-        #     'method':'get',
-        #     "url":"https://qyapi.weixin.qq.com/cgi-bin/gettoken",
-        #     "params":{
-        #         "corpid":self._corpid,
-        #         "corpsecret":self._corp_secret
-        #     }
-        # }
-        req=yaml.safe_load(open('../api/get_token.yaml'))
+        req=self.template()
         r=self.requests_http(req)
         return r
 if __name__ == '__main__':
-    r=GetToken().get_token()
-    print(r.json())
+    GetToken().template()
+    # r=GetToken().get_token()
+    # print(r.json())
